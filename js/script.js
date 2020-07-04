67a1205d55ed39a7591190c694b9dc43ebb33bfa
Arina <45823655+marsiuser@users.noreply.github.com>
2020-07-04 13:31:51 +0300

    last v

--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
 $(document).ready(function(){
+
+    // якорные ссылки
     $("#menu").on("click","a", function (event) {
         event.preventDefault();
         var id  = $(this).attr('href'),
             top = $(id).offset().top;
         $('body,html').animate({scrollTop: top}, 1500);
+    });
+
+    // кнопка Вибрати розділ
+    $('.header-section .header-menu-btn button').on('click', function() {
+        $('.container .header-section .header-links').slideToggle();
     });
 });
